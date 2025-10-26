231f5a680ce359ac8f4262834ca9e8e543103754
Daffa Izdihar <86041236+daffaizdihar@users.noreply.github.com>
2025-10-26 17:38:42 +0700

    create dos/code.py

---
+++ dos/code.py
@@ -0,0 +1,12 @@
+import flask
+from flask import Flask
+from flask import render_template as rt
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+   return rt('index.html')
+
+if __name__ == "__main__":
+   app.run()
